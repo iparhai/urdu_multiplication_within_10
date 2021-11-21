06f4b846460c40e2447926141a4ce7ba74d1dcc2
devshaffan <devshaffan@gmail.com>
2021-11-21 17:52:54 +0500

    Updates

---
+++ precache-manifest.e56a54a97153fe1540a8b15cc37b5339.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4a20ff606bf06bd8a9b",
+    "url": "/urdu_multiplication_within_10/static/css/main.5a8e527d.chunk.css"
+  },
+  {
+    "revision": "c4a20ff606bf06bd8a9b",
+    "url": "/urdu_multiplication_within_10/static/js/main.b9f2fe8f.chunk.js"
+  },
+  {
+    "revision": "f7d0ab904435db157f9d",
+    "url": "/urdu_multiplication_within_10/static/js/runtime~main.1ee418ee.js"
+  },
+  {
+    "revision": "57f3566430e9baf7d307",
+    "url": "/urdu_multiplication_within_10/static/css/2.b71b4eb5.chunk.css"
+  },
+  {
+    "revision": "57f3566430e9baf7d307",
+    "url": "/urdu_multiplication_within_10/static/js/2.0676b587.chunk.js"
+  },
+  {
+    "revision": "0043b26c368631ed46ee",
+    "url": "/urdu_multiplication_within_10/static/js/3.89406c5b.chunk.js"
+  },
+  {
+    "revision": "91412d063f2fde22edf3a015fa63a786",
+    "url": "/urdu_multiplication_within_10/static/media/removeItem.91412d06.mp3"
+  },
+  {
+    "revision": "2fd9ac2208c833754484473decfc84a1",
+    "url": "/urdu_multiplication_within_10/static/media/drop.2fd9ac22.wav"
+  },
+  {
+    "revision": "cabc22c6e80bc043c10b098b6b8854bb",
+    "url": "/urdu_multiplication_within_10/static/media/bowl.cabc22c6.png"
+  },
+  {
+    "revision": "a0d6b13874a29393b0f8252f2adb5fa7",
+    "url": "/urdu_multiplication_within_10/static/media/grocerySound.a0d6b138.mp3"
+  },
+  {
+    "revision": "57a2639bd6d80374aaf0714a48200f92",
+    "url": "/urdu_multiplication_within_10/static/media/10.57a2639b.svg"
+  },
+  {
+    "revision": "a7f0e4e3c9b56642615788ea350047d6",
+    "url": "/urdu_multiplication_within_10/static/media/1.a7f0e4e3.svg"
+  },
+  {
+    "revision": "424d18986561c7039221eb97a66aef6b",
+    "url": "/urdu_multiplication_within_10/static/media/tostartpressbutton.424d1898.mp3"
+  },
+  {
+    "revision": "8efff185f64e4259ae68a4074dbfb0e6",
+    "url": "/urdu_multiplication_within_10/static/media/321go.8efff185.mp3"
+  },
+  {
+    "revision": "09b0b7bb4e8aad40481825ec9d91d223",
+    "url": "/urdu_multiplication_within_10/static/media/rightAnswer.09b0b7bb.mp3"
+  },
+  {
+    "revision": "ddcbc59b2917e3dbeb737fca1e3ede4e",
+    "url": "/urdu_multiplication_within_10/static/media/wrongAnswer.ddcbc59b.mp3"
+  },
+  {
+    "revision": "74562e5d9b36cc740a89848ba488bf7d",
+    "url": "/urdu_multiplication_within_10/static/media/rooster.74562e5d.png"
+  },
+  {
+    "revision": "40d8e09c1bc1e01df2f2d0350718c089",
+    "url": "/urdu_multiplication_within_10/static/media/_1.40d8e09c.mp3"
+  },
+  {
+    "revision": "65a6b97dc7801719b2d140c796276265",
+    "url": "/urdu_multiplication_within_10/static/media/_2.65a6b97d.mp3"
+  },
+  {
+    "revision": "6bd419110b7fb9b6b44fd2f477a35abc",
+    "url": "/urdu_multiplication_within_10/static/media/_3.6bd41911.mp3"
+  },
+  {
+    "revision": "ae5c58d75da67259d8f8a689eb4040cd",
+    "url": "/urdu_multiplication_within_10/static/media/_4.ae5c58d7.mp3"
+  },
+  {
+    "revision": "fa32eb88ca708bfdd1f506d0d6ad7163",
+    "url": "/urdu_multiplication_within_10/static/media/_5.fa32eb88.mp3"
+  },
+  {
+    "revision": "639f363f9c8589d7472737d7d86ca22b",
+    "url": "/urdu_multiplication_within_10/static/media/_6.639f363f.mp3"
+  },
+  {
+    "revision": "7fc0455545ca15ac611aabfd2aa9d5fc",
+    "url": "/urdu_multiplication_within_10/static/media/_7.7fc04555.mp3"
+  },
+  {
+    "revision": "9c9b9bcb08b586e22914148c5f69bcd6",
+    "url": "/urdu_multiplication_within_10/static/media/_8.9c9b9bcb.mp3"
+  },
+  {
+    "revision": "fc30493cbaf5493f1204486e9e9cbbf1",
+    "url": "/urdu_multiplication_within_10/static/media/_9.fc30493c.mp3"
+  },
+  {
+    "revision": "594c0b0ed7da5faa738c12be102b3018",
+    "url": "/urdu_multiplication_within_10/static/media/_10.594c0b0e.mp3"
+  },
+  {
+    "revision": "8164e48c115fae4bb874ed0c63b40cbc",
+    "url": "/urdu_multiplication_within_10/static/media/instructions.8164e48c.gif"
+  },
+  {
+    "revision": "68818806d9bf0ab34bf79965ea51711d",
+    "url": "/urdu_multiplication_within_10/static/media/background.68818806.gif"
+  },
+  {
+    "revision": "b512cd285a5ca16e6e3ec0c535047a76",
+    "url": "/urdu_multiplication_within_10/static/media/background2.b512cd28.gif"
+  },
+  {
+    "revision": "7360a269fa89ec8922a9fb9354820523",
+    "url": "/urdu_multiplication_within_10/static/media/sound.7360a269.png"
+  },
+  {
+    "revision": "f02ac89a44b28acd80cb6b0399adbf8f",
+    "url": "/urdu_multiplication_within_10/static/media/mute.f02ac89a.png"
+  },
+  {
+    "revision": "637a398be477ada4116db81181113716",
+    "url": "/urdu_multiplication_within_10/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/urdu_multiplication_within_10/precache-manifest.e56a54a97153fe1540a8b15cc37b5339.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/urdu_multiplication_within_10/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
